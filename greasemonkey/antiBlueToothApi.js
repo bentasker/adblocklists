9e1e70be9a8c35eb8bc28e5a54e9c991a4a32edc
B Tasker <github@bentasker.co.uk>
2018-03-02 10:04:32 +0000

    Forgot I even had these. Track some greasemonkey scripts used to help block ads/tracking

---
+++ greasemonkey/antiBlueToothApi.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Anti-Bluetooth API
+// @namespace    *
+// @description  I've no need or interest in having the bluetooth API. Needs to be tested on an up-to-date Chrome to check whether this works
+// @author       B Tasker
+// @match        *
+// @grant        none
+// @version 	 1.0
+// @downloadURL  https://www.bentasker.co.uk/adblock/greasemonkey/antiBlueToothApi.js
+// @updateURL    https://www.bentasker.co.uk/adblock/greasemonkey/antiBlueToothApi.js
+// ==/UserScript==
+
+Object.defineProperty(window.navigator, 'bluetooth', {value: function requestDevice {console.log('Bluetooth access attempt');}});
+
+console.log("Loaded");
---
+++ greasemonkey/antiPopAds2017.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Anti-PopAds
+// @namespace    *
+// @description  PopAds are slimey fucks who won't honour obvious attempts to deny their attempts to run code on _our_ machines. Override some of the crap they use
+// @author       B Tasker
+// @match        *
+// @grant        none
+// @version 	 1.2
+// @downloadURL  https://www.bentasker.co.uk/adblock/greasemonkey/antiPopAds2017.js
+// @updateURL    https://www.bentasker.co.uk/adblock/greasemonkey/antiPopAds2017.js
+// ==/UserScript==
+
+/* PopAds can very seriously go fuck themselves */
+Object.defineProperty(window, '_pao', {value: function parse() {console.log('PopAds Detected');}});
+Object.defineProperty(window, '_pop',{value: function parse() {console.log('PopAdsDetected');}} );
+Object.defineProperty(window, 'popns',{value: function parse() {console.log('PopAdsDetected');}} );
+
+console.log("Loaded");
