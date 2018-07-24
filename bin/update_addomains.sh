72fab18c31e566f9eea6d92d0a19f100b4de6404
Ben Tasker <btasker@swiftserve.com>
2018-07-24 10:49:31 +0100

    Received email to say that player.h-cdn.com was added erroneously.
    Does appear to have been removed from one of the upstreams, so force removing (as it's still in the minerdomain list)

--- bin/update_addomains.sh
+++ bin/update_addomains.sh
@@ -9,7 +9,12 @@ cd "$1"
 wget -O autolist.txt "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=unbound;showintro=0&mimetype=plaintext"
 wget -O minerdomains.txt https://raw.githubusercontent.com/Marfjeh/coinhive-block/master/domains 
 
-cat minerdomains.txt | while read -r domain
+# TODO - should move excludes into a dedicated file, but setting this up in a hurry
+sed -i 's/player.h-cdn.com/#player.h-cdn.com/g' minerdomains.txt
+
+
+
+cat minerdomains.txt | egrep -v -e '^#|^$' | while read -r domain
 do
 
 cat << EOM >> autolist.txt
