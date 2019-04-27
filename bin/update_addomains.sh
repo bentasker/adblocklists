76710aaaeb4426c22eb6f9d779e9f5e8c6ceff89
B Tasker <github@bentasker.co.uk>
2019-04-28 00:53:40 +0100

    Reduce the risk of duplicates when building autolist.txt

--- bin/update_addomains.sh
+++ bin/update_addomains.sh
@@ -6,22 +6,78 @@
 
 cd "$1"
 
-wget -O autolist.txt "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=unbound;showintro=0&mimetype=plaintext"
+wget -O autolist.tmp.txt "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=unbound;showintro=0&mimetype=plaintext"
 wget -O minerdomains.txt https://raw.githubusercontent.com/Marfjeh/coinhive-block/master/domains 
 
 # TODO - should move excludes into a dedicated file, but setting this up in a hurry
 sed -i 's/player.h-cdn.com/#player.h-cdn.com/g' minerdomains.txt
 
+# Strip names out of the autolist so we can process and remove duplicates
+
+grep "local-zone" autolist.tmp.txt | cut -d\  -f2 | sed 's/"//g' > autolist.doms.txt
+
 
 
 cat minerdomains.txt | egrep -v -e '^#|^$' | while read -r domain
 do
+    echo "$domain" >> autolist.doms.txt
+done
 
-cat << EOM >> autolist.txt
+
+cat config/manualblock.txt | egrep -v -e '^#' | while read -r domain
+do
+    echo "$domain" >> autolist.doms.txt
+done
+
+
+
+
+> autolist.zones.txt
+cat config/manualzones.txt | egrep -v -e '^#' | while read -r domain
+do
+    echo "$domain" >> autolist.zones.txt
+done
+
+# Check for any domains blocked in manualpages (i.e. no variables and no path specified)
+egrep -v -e '/|\$' config/manualpages.txt | egrep -v -e '^#' | sed 's/www\.//g' | while read -r domain
+do
+    echo "$domain" >> autolist.zones.txt
+done
+
+
+# Finally process the list, and use it to build the new autolist
+cat autolist.doms.txt | sort | uniq | egrep -v -e '^$' | while read -r domain
+do
+
+# Check if the domain exists within a zone that'll be blocked
+egrep -v -e "^${domain#*.}|^$domain" autolist.zones.txt > /dev/null
+if [ "$?" == "1" ]
+then
+
+cat << EOM >> autolist.build.txt
+local-data: "$domain A 127.0.0.1"
+local-data: "$domain IN AAAA ::1"
+EOM
+
+fi
+
+done
+
+# Block the various zones
+cat autolist.zones.txt | sort | uniq | egrep -v -e '^$' | while read -r domain
+do
+cat << EOM >> autolist.build.txt
 local-zone: "$domain" redirect
 local-data: "$domain A 127.0.0.1"
+local-data: "$domain IN AAAA ::1"
 EOM
 
 done
+
+mv autolist.build.txt autolist.txt
+
+# Tidy up
+rm -f autolist.zones.txt autolist.doms.txt autolist.tmp.txt
+
 
 "$1/bin/build_abp.sh"
