084b2684de537b543514b7bd1c74038572468252
B <github@bentasker.co.uk>
2019-06-10 10:14:38 +0100

    NOOP, whitespace tweaks to make it more readable (didn't help much...)
    Will do better later

--- bin/update_addomains.sh
+++ bin/update_addomains.sh
@@ -2,8 +2,6 @@
 #
 # Update the ads domain list
 
-
-
 cd "$1"
 
 wget -O autolist.tmp.txt "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=unbound;showintro=0&mimetype=plaintext"
@@ -13,16 +11,14 @@ wget -O minerdomains.txt https://raw.githubusercontent.com/Marfjeh/coinhive-bloc
 sed -i 's/player.h-cdn.com/#player.h-cdn.com/g' minerdomains.txt
 
 # Strip names out of the autolist so we can process and remove duplicates
-
 grep "local-zone" autolist.tmp.txt | cut -d\  -f2 | sed 's/"//g' > autolist.doms.txt
 
 
-
+# Start compiling the autolist
 cat minerdomains.txt | egrep -v -e '^#|^$' | while read -r domain
 do
     echo "$domain" >> autolist.doms.txt
 done
-
 
 cat config/manualblock.txt | egrep -v -e '^#' | while read -r domain
 do
@@ -30,9 +26,10 @@ do
 done
 
 
-
-
+# Truncate the existing file
 > autolist.zones.txt
+
+# Iterate over the zones and start compiling the zone blocker
 cat config/manualzones.txt | egrep -v -e '^#' | while read -r domain
 do
     echo "$domain" >> autolist.zones.txt
@@ -44,30 +41,32 @@ do
     echo "$domain" >> autolist.zones.txt
 done
 
-> blockeddomains.build.txt
+
 # Finally process the list, and use it to build the new autolist
+> blockeddomains.build.txt
 cat autolist.doms.txt | sort | uniq | egrep -v -e '^$' | while read -r domain
 do
-echo "$domain" >> blockeddomains.build.txt
+    echo "$domain" >> blockeddomains.build.txt
 
 
-# Check if the domain exists within a zone that'll be blocked
-egrep -v -e "^${domain#*.}|^$domain" autolist.zones.txt > /dev/null
-if [ "$?" == "1" ]
-then
+    # Check if the domain exists within a zone that'll be blocked
+    egrep -v -e "^${domain#*.}|^$domain" autolist.zones.txt > /dev/null
+    if [ "$?" == "1" ]
+    then
 
 cat << EOM >> autolist.build.txt
 local-data: "$domain A 127.0.0.1"
 local-data: "$domain IN AAAA ::1"
 EOM
 
-fi
+    fi
 
 done
 
 # Block the various zones
 cat autolist.zones.txt | sort | uniq | egrep -v -e '^$' | while read -r domain
 do
+
 cat << EOM >> autolist.build.txt
 local-zone: "$domain" redirect
 local-data: "$domain A 127.0.0.1"
@@ -81,6 +80,5 @@ mv blockeddomains.build.txt blockeddomains.txt
 
 # Tidy up
 rm -f autolist.zones.txt autolist.doms.txt autolist.tmp.txt blockeddomains.build.txt
-
 
 "$1/bin/build_abp.sh"
