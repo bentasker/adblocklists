d841c2572a60804f1627c3f49be6e7f0f1aa352b
B Tasker <github@bentasker.co.uk>
2020-05-02 11:33:40 +0100

    ADBLK-21 add script to pull down and consume the regex list

---
+++ bin/pihole_apply_regexes.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#
+# Pull down a list of blocked zones (originally implemented in ADBLK-12)
+# then translate them into regex's to feed into Pihole
+#
+# Add on the ADBLK-21 regexes
+#
+# This is better than blocking upstream, as a block in FTL will
+# result in the adblock counter increasing
+#
+# Copyright (c) 2019 B Tasker
+# Released under GNU GPL V3 - see https://www.gnu.org/licenses/gpl-3.0.txt
+# 
+#
+
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+# Create a temp file
+OUT="$(mktemp)"
+> $OUT
+
+
+# Fetch the blocked zone file
+for zone in `curl -s "https://www.bentasker.co.uk/adblock/blockedzones.txt"`
+do
+    regex=$(echo "$zone" | sed 's~\.~\\\.~g')
+    echo "^.+\.$regex\$" >> $OUT
+done
+
+for regex in `curl -s "https://www.bentasker.co.uk/adblock/regex_blocks.txt" | egrep -v -e '^$|^#' ` 
+do
+    echo "$regex" >> $OUT
+done
+
+# Remove any duplicates
+OUT2="$(mktemp)"
+cat $OUT | sort | uniq > $OUT2
+mv $OUT2 $OUT
+
+
+count=$(wc -l "$OUT" | cut -d\  -f1)
+
+if [ $count -gt 0 ]
+then
+
+    # Check the files actually differ
+    cd /etc/pihole
+    diff -u "$OUT" regex.list > /dev/null
+    if [ "$?" == "1" ]
+    then
+        # Files differ. Make a backup
+        cp -n regex.list regex.list.old
+        cat "$OUT" > regex.list
+
+        # Tell Pihole to reload the regexes
+        echo ">recompile-regex" | nc -q1 localhost 4711
+    fi
+fi
+
+rm -f "$OUT"
+
+
+
+
+
+
+
